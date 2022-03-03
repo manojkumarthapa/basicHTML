4406598e62528bd6b1028a2fe52d883e16001ad3
Manoj Kumar Thapa <genjithapa182@hotmail.com>
2022-03-03 12:28:49 +0000

    first commit

---
+++ assets/app.js
@@ -0,0 +1,54 @@
+$('#imageGranny').click(()=>{
+    Swal.fire({
+        title: 'Granny Smith!',
+        text: 'Who is granny smith then?',
+        imageUrl: 'assets/img/granny.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+    })
+})
+
+$('#imageOrange').click(()=>{
+    Swal.fire({
+        title: 'How do oranges communicate with each other?',
+        text: 'They speak in Mandarin.',
+        imageUrl: 'assets/img/annoying_orange.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+    })
+})
+
+$('#imageApple').click(()=>{
+    Swal.fire({
+        title: "What kind of apple isn't an apple?",
+        text: 'A pineapple.',
+        imageUrl: 'assets/img/pineapple.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+    })
+})
+
+$('#imageBanana').click(()=>{
+    Swal.fire({
+        title: "Why did the men lose his job in a fruit packing firm?",
+        text: 'He kept throwing the bent bananas away.',
+        imageUrl: 'assets/img/peanutbutterjelly.gif',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+    })
+})
+
+$('#imageGrapes').click(()=>{
+    Swal.fire({
+        title: "Why did Mrs.Grape leave her family?",
+        text: 'She was tired of raisin kids.',
+        imageUrl: 'assets/img/grapes.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+    })
+})
